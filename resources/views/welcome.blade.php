34a316cf4578510ab514741b30b28e3b1da0268a
Alessandro <marzucoalessandro1@gmail.com>
2021-03-04 19:14:33 +0100

    sezione header del sito

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
 
         <link rel="stylesheet" href="css/app.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet">
     </head>
     <body>
       <!-- costruisco la navbar -->
@@ -41,7 +42,26 @@
         </div>
       </nav>
       <header>
-        
+        <div class="comic">
+          <div class="banner">
+            <h4>comics & graphic novel</h4>
+          </div>
+          <div class="banner_link">
+            <a href="#">this week</a>
+            <a href="#">last week</a>
+            <a href="#">next week</a>
+            <a href="#">upcoming</a>
+            <a href="#">see all</a>
+          </div>
+
+          <div class="card_one">
+            <div class="flag">comic book</div>
+            <img src="/img/batman.jpg" alt="batman">
+            <p class="small_text">batman #106</p>
+            <p class="available">available now</p>
+          </div>
+
+        </div>
       </header>
 
 
