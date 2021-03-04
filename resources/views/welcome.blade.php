71ca98f14b03fd88a8a282d9a11a9805931a9324
Alessandro <marzucoalessandro1@gmail.com>
2021-03-04 23:01:47 +0100

    main homesite completo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
 
           <div class="card_one">
             <span class="flag">comic book</span>
-            <img class="first_img" src="https://www.dccomics.com/sites/default/files/styles/comics320x485/public/comic-covers/2021/02/BM_106_300-001_HD_60343a048f2af5.78911718.jpg?itok=2PO-YXXX" alt="batman">
+            <img class="" src="https://www.dccomics.com/sites/default/files/styles/comics320x485/public/comic-covers/2021/02/BM_106_300-001_HD_60343a048f2af5.78911718.jpg?itok=2PO-YXXX" alt="batman">
             <p class="small_text">batman #106</p>
             <p class="available">available now</p>
           </div>
@@ -84,9 +84,103 @@
             <p class="small_text">sensational wonder woman #9</p>
             <p class="available">available now</p>
           </div>
-
-        </div>
       </header>
+
+      <main>
+        <div class="container mb-6">
+          <div class="row ">
+            <div class="col-9">
+              <h3>must reads</h3>
+              <section class="must_read">
+                <div class="card_two">
+                  <img src="https://www.dccomics.com/sites/default/files/mustRead_DCC_5_v2_5f6b8b49bfd6a9.60761869.jpg" alt="">
+                  <h4>get the inside track on all things dc</h4>
+                  <h2>dc connect</h3>
+                  <p>The monthly, downloadable catalog of DC' comics,graphic novel,collectibles and more</p>
+                </div>
+                <div class="card_two">
+                  <img src="https://www.dccomics.com/sites/default/files/MUSTREAD_20190114_SandmanUniverse_5c3e4bab834ac9.58189616.jpg" alt="">
+                  <h4>from the mind of neil gaiman</h4>
+                  <h2>the sandman universe</h3>
+                  <p>The monthly, downloadable catalog of DC' comics,graphic novel,collectibles and more</p>
+                </div>
+                <div class="card_two">
+                  <img src="https://www.dccomics.com/sites/default/files/MUSTREAD_20190114_WonderComics_5c3e4b8d091c94.82772322.jpg" alt="">
+                  <h4>these kids are all fight</h4>
+                  <h2>wonder comics</h3>
+                  <p>Brian Michael Bendis shines the spotlight on DC's youngest heroes!</p>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <section>
+          <div class="collection">
+            <div class="container">
+              <div class="row">
+                <div class="col-12 flex">
+                  <div class="tag">
+                    <h4 class="current">current series</h4>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2020/11/SeriesThumb7_AmericanVampire1976_5fc5637bb970f3.51703648.jpg?itok=dFqZbX0C" alt="">
+                    <h5>american vampire 1976</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2018/10/SeriesThumb5_Batman_5bd218c153e9a0.67359915.jpg?itok=DRQko-YJ" alt="">
+                    <h5>batman</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2020/11/SeriesThumb7_BMBW_5fc5658b1171c4.10950786.jpg?itok=vdjPO7g4" alt="">
+                    <h5>batman black and white</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2019/09/SeriesThumb5_BMvRas_5d82c7892f7e63.62526030.jpg?itok=gI2tUr8A" alt="">
+                    <h5>batman vs ra's al ghul</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2020/11/SeriesThumb7_BMCW_5fc56629b44c39.86920586.jpg?itok=woc42hJR" alt="">
+                    <h5>batman/catwoman</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/default_images/DC_placeholder_series_573b3c5dac0485.40480935.jpg?itok=oRZ6eTCO" alt="">
+                    <h5>batman:urban legend</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2020/11/SeriesThumb7_BMWKHQ_5fc5675de13b69.95483216.jpg?itok=rjGyMJz4" alt="">
+                    <h5>batman:white night</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2018/06/SeriesThumb5_catwoman_5b1ec42eec63e2.07910716.jpg?itok=EJyrm6sv" alt="">
+                    <h5>catwoman</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/default_images/DC_placeholder_series_573b3c5dac0485.40480935.jpg?itok=oRZ6eTCO" alt="">
+                    <h5>crime syndicate</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2020/06/SeriesThumb6_DCConnect_5ef2a2f4f34d10.17317418.jpg?itok=htCFuMwa" alt="">
+                    <h5>dc connect</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2021/02/SeriesThumb8_DCLoveBattlefield_60305d0e0a6d74.37220374.jpg?itok=jY5TpOHt" alt="">
+                    <h5>dc love is a battle</h5>
+                  </div>
+                  <div class="card_three">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/current_series_grid_item192x192/public/series-covers/2019/11/SeriesThumb5_FarSector_5dcb5fc5009773.36625144.jpg?itok=DzBciYp0" alt="">
+                    <h5>far sector</h5>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+      </main>
+
+
 
 
     </body>
